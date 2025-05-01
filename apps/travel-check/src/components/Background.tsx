0c317585dbb112cb8f048fbac6354a4f3ddaafa1
jamooooong <andro606@naver.com>
2025-05-01 20:57:16 +0900

    Feat [#3] update Background

--- apps/travel-check/src/components/Background.tsx
+++ apps/travel-check/src/components/Background.tsx
@@ -1,3 +1,15 @@
+import Cloud from "../assets/background/Cloud.svg";
+import Land from "../assets/background/Land.svg";
+
 export default function Background() {
-  return <div className="h-screen bg-linear-to-t from-[#00DAEB] to-[#004185]"></div>;
+  return (
+    <div className="h-screen bg-linear-to-t from-[#00DAEB] to-[#004185]">
+      <img src={Cloud} alt="cloud" className="absolute top-0 left-0 mt-16 w-full" />
+      <img
+        src={Land}
+        alt="land"
+        className="absolute bottom-0 left-1/2 h-auto w-4/5 -translate-x-1/2 transform"
+      />
+    </div>
+  );
 }
