e480231b812a830adea0de870d6dbd51dc8d8bb1
jamooooong <andro606@naver.com>
2025-05-01 20:58:23 +0900

    Feat [#3] make sub routing

--- apps/travel-check/src/routes/index.tsx
+++ apps/travel-check/src/routes/index.tsx
@@ -1,5 +1,6 @@
+// 시작화면
 import { createFileRoute } from "@tanstack/react-router";
-import { StartButton } from "@travel-check-client/ui";
+import { StartButton, ImageTypoLogo, ImageCharacter } from "@travel-check-client/ui";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
@@ -7,8 +8,15 @@ export const Route = createFileRoute("/")({
 
 function RouteComponent() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <p className="font-dungGeunMo text-2xl">여행췍</p>
+    <div className="flex flex-col items-center justify-center pt-20">
+      <p className="mb-4 text-xs text-white"> 쉽고 빠른 여행 체크리스트 공유</p>
+      <div className="mb-8">
+        <ImageTypoLogo />
+      </div>
+      <div className="mb-36">
+        <ImageCharacter />
+      </div>
+
       <div className="">
         <StartButton onClick={() => alert("여행췍 시작")} />
       </div>
