4e2078ff5d6911e26739e005cbd860c8b576ead0
jamooooong <andro606@naver.com>
2025-06-03 16:52:49 +0900

    Feat [#9] Link components setting

--- apps/travel-check/src/routes/link.tsx
+++ apps/travel-check/src/routes/link.tsx
@@ -1,5 +1,7 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { ButtonLg, InputText, PixelBox, TitleBox } from "@travel-check-client/ui";
+
+import Header from "@components/Header";
+import InfoLink from "@LinkPage/InfoLink";
 
 export const Route = createFileRoute("/link")({
   component: RouteComponent,
@@ -7,24 +9,9 @@ export const Route = createFileRoute("/link")({
 
 function RouteComponent() {
   return (
-    <div className="flex h-full flex-col items-center justify-between py-4">
-      <TitleBox title="여행 링크 복사" />
-      <div>
-        <PixelBox>
-          <div className="mb-2 flex w-full flex-col gap-2">
-            <div className="mb-2">
-              <p className="mb-1"> 여행 입장 링크</p>
-              <InputText />
-            </div>
-            <ButtonLg color="red" onClick={() => {}}>
-              링크 복사하기
-            </ButtonLg>
-            <ButtonLg color="green" onClick={() => {}}>
-              여행 체크리스트 접속
-            </ButtonLg>
-          </div>
-        </PixelBox>
-      </div>
-    </div>
+    <>
+      <Header />
+      <InfoLink />
+    </>
   );
 }
