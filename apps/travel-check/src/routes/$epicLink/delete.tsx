3dbfbc7ef8807674010cdfe19a1ab46175381674
jamooooong <andro606@naver.com>
2025-06-04 00:48:13 +0900

    Feat [#9] delete components setting

--- apps/travel-check/src/routes/$epicLink/delete.tsx
+++ apps/travel-check/src/routes/$epicLink/delete.tsx
@@ -1,5 +1,8 @@
+import Header from "@components/Header";
+import PersonalCheckList from "@components/PersonalCheck/PersonalCheckList";
+import ScrollArea from "@components/ScrollArea";
+import TeamCheckList from "@components/TeamCheck/TeamCheckList";
 import { createFileRoute } from "@tanstack/react-router";
-import { PixelBox, TitleBox, InputAddList, ButtonLg, ButtonMed } from "@travel-check-client/ui";
 
 export const Route = createFileRoute("/$epicLink/delete")({
   component: RouteComponent,
@@ -8,24 +11,11 @@ export const Route = createFileRoute("/$epicLink/delete")({
 function RouteComponent() {
   return (
     <>
-      <div className="flex flex-col items-center justify-center gap-4 pt-4">
-        <TitleBox title="여행이름" />
-        <PixelBox>
-          <div className="h-[30rem] w-full p-0">
-            <p className="w-full text-base">단체 체크리스트(0/0)</p>
-
-            <p className="w-full text-base">개인 체크리스트(0/0)</p>
-          </div>
-        </PixelBox>
-        <div className="flex flex-row gap-4">
-          <ButtonMed color="red" onClick={() => {}}>
-            취소
-          </ButtonMed>
-          <ButtonMed color="green" onClick={() => {}}>
-            저장
-          </ButtonMed>
-        </div>
-      </div>
+      <Header />
+      <ScrollArea>
+        <TeamCheckList />
+        <PersonalCheckList />
+      </ScrollArea>
     </>
   );
 }
