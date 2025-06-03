08f5a72eeb797724c401b0c84f3401a429124b3a
jamooooong <andro606@naver.com>
2025-06-04 00:55:22 +0900

    Feat [#9] Member components setting

--- apps/travel-check/src/routes/$epicLink/member.tsx
+++ apps/travel-check/src/routes/$epicLink/member.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { ButtonLg, InputText, PixelBox, TitleBox } from "@travel-check-client/ui";
-import MemberList from "../../components/MemberList";
+
+import Header from "@components/Header";
+import ScrollArea from "@components/ScrollArea";
+import ParticipantManageList from "@MemberPage/ParticipantManage/ParticipantManageList";
 
 export const Route = createFileRoute("/$epicLink/member")({
   component: RouteComponent,
@@ -8,21 +10,11 @@ export const Route = createFileRoute("/$epicLink/member")({
 
 function RouteComponent() {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 pt-4">
-      <TitleBox title="여행이름" />
-      <PixelBox>
-        <div className="w-full">
-          <p className="w-full text-base">여행이름 여행 인원</p>
-          <MemberList user="박우석" />
-        </div>
-      </PixelBox>
-      <PixelBox>
-        <p className="w-full text-base">여행 입장 링크</p>
-        <InputText />
-        <ButtonLg color="red" onClick={() => {}}>
-          링크 복사하기
-        </ButtonLg>
-      </PixelBox>
-    </div>
+    <>
+      <Header />
+      <ScrollArea>
+        <ParticipantManageList />
+      </ScrollArea>
+    </>
   );
 }
